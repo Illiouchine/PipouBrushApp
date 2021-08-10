697d36dbd4bf55bb730c63fa5a6527f267fcf260
William Guillermin <guillermin.w@gmail.com>
2021-08-10 13:23:00 +0200

    Some nasty shit done before vacation

--- app/src/main/java/com/illiouchine/toothbrush/feature/brushing/ui/composable/WaitingContent.kt
+++ app/src/main/java/com/illiouchine/toothbrush/feature/brushing/ui/composable/WaitingContent.kt
@@ -1,20 +1,24 @@
 package com.illiouchine.toothbrush.feature.brushing.ui.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.illiouchine.toothbrush.feature.brushing.brushDuration
-import com.illiouchine.toothbrush.ui.ToothBrushTheme
 import com.illiouchine.toothbrush.feature.main.composable.MyButton
-import com.illiouchine.toothbrush.ui.composable.TimerText
+import com.illiouchine.toothbrush.ui.ToothBrushTheme
 import com.illiouchine.toothbrush.ui.composable.chrono.Chrono
+import com.illiouchine.toothbrush.ui.utils.assetsToBitmap
 import java.util.concurrent.TimeUnit
 import kotlin.time.ExperimentalTime
 
@@ -22,13 +26,26 @@ import kotlin.time.ExperimentalTime
 @ExperimentalTime
 @Composable
 fun WaitingContent(
-    onStartTimerClick: ()-> Unit = {}
-){
+    onStartTimerClick: () -> Unit = {}
+) {
+    val context = LocalContext.current
+    val bitmap: ImageBitmap? = remember {
+        context
+            .assetsToBitmap("brossage_dents_redimension.jpg")
+            ?.asImageBitmap()
+    }
+
     Surface {
+        bitmap?.let {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                bitmap = it,
+                contentDescription = "BG"
+            )
+        }
         Column(
             modifier = Modifier
-                .fillMaxSize()
-            ,
+                .fillMaxSize(),
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -42,7 +59,7 @@ fun WaitingContent(
             )
             Spacer(Modifier)
             //VideoExoPlayer()
-            MyButton(text = "Go !"){
+            MyButton(text = "Go !") {
                 onStartTimerClick()
             }
             Spacer(Modifier)
@@ -68,7 +85,7 @@ fun WaitingStartBrushScreenLight() {
 @Composable
 @ExperimentalTime
 fun WaitingStartBrushScreenDark() {
-    ToothBrushTheme (darkTheme = true) {
+    ToothBrushTheme(darkTheme = true) {
         WaitingContent()
     }
 }
---
+++ app/src/main/java/com/illiouchine/toothbrush/ui/utils/BitmapExt.kt
@@ -0,0 +1,16 @@
+package com.illiouchine.toothbrush.ui.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.io.IOException
+
+fun Context.assetsToBitmap(fileName: String): Bitmap? {
+    return try {
+        with(assets.open(fileName)){
+            BitmapFactory.decodeStream(this)
+        }
+    }catch (e: IOException) {
+        null
+    }
+}
